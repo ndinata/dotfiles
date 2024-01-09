45648286f4a4f11d8f8eb17fe00b378263d8c7a4
Nico Dinata <nicodnt@icloud.com>
2024-01-09 20:40:04 +0700

    replace $HOME with `~` expansion

--- drip/src/parse.rs
+++ drip/src/parse.rs
@@ -1,6 +1,8 @@
-use std::path::PathBuf;
+use std::ffi::OsStr;
+use std::ops::Deref;
+use std::path::{Path, PathBuf};
 
-use knuffel::Decode;
+use knuffel::{traits::ErrorSpan, Decode, DecodeScalar};
 use miette::Diagnostic;
 
 /// Error type for when parsing a `Recipe` fails.
@@ -53,14 +55,67 @@ pub struct Formula {
 #[non_exhaustive]
 pub enum Postinstall {
     /// Copies `.0` to `.1`.
-    Cp(#[knuffel(argument)] PathBuf, #[knuffel(argument)] PathBuf),
+    Cp(
+        #[knuffel(argument)] ExpandedPath,
+        #[knuffel(argument)] ExpandedPath,
+    ),
 
     /// Downloads `.0` to `.1`.
-    Dl(#[knuffel(argument)] String, #[knuffel(argument)] PathBuf),
+    Dl(
+        #[knuffel(argument)] String,
+        #[knuffel(argument)] ExpandedPath,
+    ),
 
     /// Appends `.0` to file in `.1`.
-    Echo(#[knuffel(argument)] String, #[knuffel(argument)] PathBuf),
+    Echo(
+        #[knuffel(argument)] String,
+        #[knuffel(argument)] ExpandedPath,
+    ),
 
     /// Runs some `fish` command `.0`.
     Fish(#[knuffel(argument)] String),
+}
+
+/// Newtype for a `PathBuf` with its `~` expanded into the full home dir path.
+///
+/// The expansion actually takes place when doing `raw_decode()`.
+#[derive(Debug)]
+pub struct ExpandedPath(PathBuf);
+
+impl Deref for ExpandedPath {
+    type Target = PathBuf;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl AsRef<Path> for ExpandedPath {
+    fn as_ref(&self) -> &Path {
+        &self.0
+    }
+}
+
+impl AsRef<OsStr> for ExpandedPath {
+    fn as_ref(&self) -> &OsStr {
+        self.0.as_ref()
+    }
+}
+
+impl<S: ErrorSpan> DecodeScalar<S> for ExpandedPath {
+    fn raw_decode(
+        value: &knuffel::span::Spanned<knuffel::ast::Literal, S>,
+        ctx: &mut knuffel::decode::Context<S>,
+    ) -> Result<Self, knuffel::errors::DecodeError<S>> {
+        let pathbuf = PathBuf::raw_decode(value, ctx)?;
+
+        Ok(Self(simple_home_dir::expand_tilde(pathbuf).unwrap()))
+    }
+
+    fn type_check(
+        type_name: &Option<knuffel::span::Spanned<knuffel::ast::TypeName, S>>,
+        ctx: &mut knuffel::decode::Context<S>,
+    ) {
+        PathBuf::type_check(type_name, ctx)
+    }
 }
